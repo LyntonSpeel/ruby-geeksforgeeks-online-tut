461c16ed523cc1b723f8b4958425764fbc4d9e73
LyntonSpeel <lynton@nexl.cloud>
2022-07-26 11:13:49 +0200

    loops

---
+++ control_statements/loops.rb
@@ -0,0 +1,48 @@
+# while loop
+
+x = 4
+
+while x >= 1
+    puts "GeeksForGeeks"
+    x -= 1
+end
+puts
+
+# for loop
+
+i = "Sudo Placements"
+
+for a in 1..5 do
+    print a, " ", i
+    puts
+end
+puts
+
+arr = ["GFG", "G4G", "Geeks", "Sudo"]
+
+for i in arr do
+    puts i
+end
+puts
+
+# do..while loop
+
+loop do
+    puts "GeeksForGeeks"
+
+    val = '7'
+
+    if val == '7'
+        break
+    end
+end
+puts
+
+# until loop
+
+var = 7
+
+until var == 11 do
+    puts var * 10
+    var += 1
+end
